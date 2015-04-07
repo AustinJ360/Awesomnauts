b922747a581639373476aaae6116a7324747fa37
Austin Juntra <AustinJuntra@gmail.com>
2015-04-07 09:14:29 -0700

    finish php video 55

--- php/controller/login-user.php
+++ php/controller/login-user.php
@@ -1,24 +1,31 @@
 
 <?php
-	require_once(__DIR__ . "/../model/config.php");
-
-	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
-	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);  
-
-	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
-
-	if($query->num_rows == 1){
-		$row = $query->fetch_array();
-
-		if($row["password"] === crypt($password, $row["salt"])){
-			$_SESSION["authenticated"] = true;
-			echo "<p>Login Successful!</p>";
-		}
-		else{
-			echo "<p>Invalid username and password</p>";
-		}
-	}
-	else{
-		echo"<p>Invalid username and password</p>";
-	}
-?>
+	  require_once(__DIR__ . "/../model/config.php");
+	  $array = array(
+	  	'exp'=> '',
+	  	'exp1'=> '',
+	  	'exp2'=> '',
+	  	'exp3'=> '',
+	  	'exp4'=> '',
+	  	);
+	  $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	  $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+	  $query = $_SESSION["connection"]->query("SELECT * FROM users WHERE username = '$username'");//select the salt and password from the users table.
+	  if ($query->num_rows == 1) {
+	  	  $row = $query->fetch_array();	
+	  	  if($row["password"] === crypt($password, $row["salt"])) {
+	  	  	$_SESSION["authenticated"] = true;//authenticates the users
+	  	  	$array["exp"] = $row["exp"];
+	  	  	$array["exp1"] = $row["exp1"];
+	  	  	$array["exp2"] = $row["exp2"];
+	  	  	$array["exp3"] = $row["exp3"];
+	  	  	$array["exp4"] = $row["exp4"];
+			$_SESSION["name"] = $username; 
+	  	  	echo json_encode($array);//Logged in correctly
+	  	  } else {
+	  	  	echo "<p>Invalid username and password</p>";//tells the user whats wrong but not actually teling them ;D
+		 }
+	} else {
+	 		echo "<p>Invalid username and password</p>";
+	 	 }
+ 

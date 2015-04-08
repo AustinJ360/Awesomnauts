227eece0130709d7cdc4ed3ff8eca5c6f842b5d0
Austin Juntra <AustinJuntra@gmail.com>
2015-04-07 20:47:13 -0700

    adding mini player intermediate

--- index.php
+++ index.php
@@ -68,6 +68,7 @@
 		<script type="text/javascript" src="js/screens/spendExp.js"></script>
 	    <script type="text/javascript" src="js/screens/loadProfile.js"></script>
 	   	<script type="text/javascript" src="js/screens/newProfile.js"></script>
+	   	<script type="text/javascript" src="js/entities/MiniPlayerLocation.js"></script>
 
 
 
